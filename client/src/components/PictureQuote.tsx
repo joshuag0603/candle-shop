e10e82bf4fd1a06b506e70b6dd4e787459c1b4f0
Joshua Clennon <clennonjoshua@gmail.com>
2025-01-31 10:37:23 -0500

    updated pitureandquote component

--- client/src/components/PictureQuote.tsx
+++ client/src/components/PictureQuote.tsx
@@ -1,31 +1,56 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
-interface PictureQuoteProps{
-    imagesrc: string;
-    alttext?: string;
-    quote:string;
-    author?:string;
-
+interface PictureAndQuoteProps {
+  imageSrc: string;
+  altText?: string;
+  quote: string;
+  author?: string;
 }
 
-const PictureQuote: React.FC<PictureQuoteProps>=({
-    imagesrc,
-    alttext,
-    quote,
-    author,
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem auto;
+  max-width: 400px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+`;
+
+const Quote = styled.blockquote`
+  font-style: italic;
+  text-align: center;
+  margin-top: 1rem;
+  padding: 0 1rem;
+`;
+
+const Author = styled.cite`
+  display: block;
+  margin-top: 0.5rem;
+  font-weight: bold;
+  font-style: normal;
+`;
+
+const PictureAndQuote: React.FC<PictureAndQuoteProps> = ({
+  imageSrc,
+  altText,
+  quote,
+  author,
 }) => {
-    return (
-        <div style= {styles.container}>
-            <img src= {imagesrc} alt={alttext} style={StyleSheet.image} />
-            <blockquote style= {styles.quote}>
-                "{quote}"
-                {author && <cite style= {StyleSheet.author}>- {author}</cite>}
-                </blockquote>
-                </div>
+  return (
+    <Container>
+      <Image src={imageSrc} alt={altText} />
+      <Quote>
+        “{quote}”
+        {author && <Author>-{author}</Author>}
+      </Quote>
+    </Container>
+  );
+};
 
-            
-            )
-
-}
-
-export default PictureQuote
+export default PictureAndQuote;
