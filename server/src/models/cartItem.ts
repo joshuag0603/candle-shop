2373b6267a4da0213506ca8daa2584603e18d916
joshuag0603 <joshuag0306@gmail.com>
2025-02-02 14:43:54 -0500

    models and auth-routes update

---
+++ server/src/models/cartItem.ts
@@ -0,0 +1,34 @@
+import { Model, Sequelize, DataTypes, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey} from 'sequelize';
+
+import { Product } from './products';
+
+export class CartItem extends Model<InferAttributes<CartItem>, InferCreationAttributes<CartItem>> {
+    declare id: CreationOptional<number>;
+    declare quantity: number;
+    declare productname: ForeignKey<Product['productname']>;
+}
+
+    export function CartItemFactory(sequelize: Sequelize) {
+    CartItem.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            quantity: {  
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'cartitems',
+            sequelize,
+            timestamps: true,
+            underscored: true,
+            modelName: "cartitems"  
+        }
+    );
+
+    return CartItem;
+}
