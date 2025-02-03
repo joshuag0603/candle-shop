0464a6fb04e352f21c491ebaa05990be52d5c0d3
Wsanchez91 <williamsanchez.8055@gmail.com>
2025-02-03 14:46:32 -0500

    added the home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+import NavBar from "../components/navbar";
+import PictureAndQuote from "../components/PictureQuote";
+import { ProductInfo } from "../interface/Products";
+
+const Home: React.FC = () => {
+  const products: ProductInfo[] = [
+    {
+      id: "1",
+      productName: "Peppermint",
+      description: " ",
+      price: 15,
+      quantity: 20,
+    },
+    {
+      id: "2",
+      productName: "Hot Apple Pie",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "3",
+      productName: "Whiskey Scented Candle",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "4",
+      productName: "Caramel Popcorn",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "5",
+      productName: "Hot Apple Pie",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "6",
+      productName: "Hot Apple Pie",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "7",
+      productName: "Hot Apple Pie",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "8",
+      productName: "Hot Apple Pie",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "9",
+      productName: "Hot Apple Pie",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+    {
+      id: "10",
+      productName: "Hot Apple Pie",
+      description: " ",
+      price: 13,
+      quantity: 15,
+    },
+  ];
+
+  return (
+    <>
+      <NavBar />
+      <h1>Welcome to Our Candle Shop</h1>
+      <PictureAndQuote
+        imageSrc="https://via.placeholder.com/400"
+        altText="Inspirational Image"
+        quote="The best way to predict the future is to create it."
+        author="Peter Drucker"
+      />
+      <h2>Our Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h3>{product.productName}</h3>
+            <p>{product.description}</p>
+            <p>Price: ${product.price.toFixed(2)}</p>
+            <p>Stock: {product.quantity}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Home;
