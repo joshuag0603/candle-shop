fd38328d2fd95fcd1af6a341ce0d94924d684eac
joshuag0603 <joshuag0306@gmail.com>
2025-02-03 13:07:55 -0500

    Models Updated final

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,17 +2,19 @@ import sequelize from "../config/connection.js";
 import { CartFactory } from "./Cart.js";
 import { ProductFactory } from "./Products.js";
 import { UserFactory } from "./Users2.js";
+import { CartItemFactory } from "./CartItem.js";
 
 const Cart = CartFactory(sequelize);
 const Product = ProductFactory(sequelize);
 const User = UserFactory(sequelize);
+const CartItem = CartItemFactory(sequelize);
 
 User.hasMany(Cart, {
     onDelete: 'CASCADE',
     as: 'carts'
 });
 
-Cart.hasMany(Product, {
+Cart.hasMany(CartItem, {
     onDelete: 'CASCADE',
     as: 'products'
 });
@@ -21,8 +23,8 @@ Cart.belongsTo(User, {
     onDelete: 'CASCADE',
 });
 
-Product.belongsTo(Cart, {
+CartItem.belongsTo(Cart, {
     onDelete: 'CASCADE',
 });
 
-export {Cart, Product, User};
+export {Cart, Product, User, CartItem};
