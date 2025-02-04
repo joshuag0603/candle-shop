c05ae375dd77d6db74c1b5ba2232e2f8b813eca8
joshuag0603 <joshuag0306@gmail.com>
2025-02-04 12:58:05 -0500

    Seeds and Routes

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -1,13 +1,15 @@
 import { seedDatabase } from "./seed.js";
+
 import sequelize from "../config/connection.js";
 
 const seedAll = async (): Promise<void> => {
     try {
+        // console.log('im being hit ')
         await sequelize.sync({ force: true });
         console.log('\n----- DATABASE SYNCED -----\n');
 
         await seedDatabase();
-        console.log('\n----- PRODUCTS AND USERS SEEDED -----\n');
+        // console.log('\n----- PRODUCTS AND USERS SEEDED -----\n');
 
         process.exit(0);
     } catch (error) {
