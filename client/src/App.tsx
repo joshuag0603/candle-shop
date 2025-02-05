1b36496ba9c4d75f0061a74eb0ab97d1c34d3e3e
joshuag0603 <joshuag0306@gmail.com>
2025-02-04 19:14:48 -0500

    images

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,27 @@
-// App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from '../src/components/navbar'; // or adjust the path as needed
+// // App.tsx
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Navbar from '../src/components/navbar'; // or adjust the path as needed
 
-const Home: React.FC = () => <div><h1>Home Page</h1></div>;
-const Products: React.FC = () => <div><h1>Products Page</h1></div>;
-const Settings: React.FC = () => <div><h1>Settings</h1></div>;
-const Cart: React.FC = () => <div><h1>Cart</h1></div>;
+// const Home: React.FC = () => <div><h1>Home Page</h1></div>;
+// const Products: React.FC = () => <div><h1>Products Page</h1></div>;
+// const Settings: React.FC = () => <div><h1>Settings</h1></div>;
+// const Cart: React.FC = () => <div><h1>Cart</h1></div>;
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/products" component={Products} />
-        {/* Replace or add correct routes for About and Contact if they exist */}
-        {/* <Route path="/about" component={About} /> */}
-        {/* <Route path="/contact" component={Contact} /> */}
-      </Switch>
-    </Router>
-  );
-};
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route path="/products" component={Products} />
+//         {/* Replace or add correct routes for About and Contact if they exist */}
+//         {/* <Route path="/about" component={About} /> */}
+//         {/* <Route path="/contact" component={Contact} /> */}
+//       </Switch>
+//     </Router>
+//   );
+// };
 
-export default App;
+// export default App;
 
