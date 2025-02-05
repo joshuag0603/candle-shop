67944080be474db281479ef6601fdb63fbfde916
joshuag0603 <joshuag0306@gmail.com>
2025-02-05 09:55:41 -0500

    Postgres

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,12 +3,23 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres',
-    dialectOptions: {
-        decimalNumbers: true,
-    },
-});
+let sequelize;
+
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME || 'candleshop_db',
+      process.env.DB_USER || 'postgres',
+      process.env.DB_PW,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+            decimalNumbers: true,
+        }, 
+      },
+    );
+  }
 
 export default sequelize;
